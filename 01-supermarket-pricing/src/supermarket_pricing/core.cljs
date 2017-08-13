d8aef6006fe6a9f4a8fd082be9159098f11ee129
Larry Jones <larryj@blinds.com>
2017-08-13 17:41:53 -0500

    State for unit price

--- 01-supermarket-pricing/src/supermarket_pricing/core.cljs
+++ 01-supermarket-pricing/src/supermarket_pricing/core.cljs
@@ -9,28 +9,39 @@
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:bean-pricing 
-                          {:header "Bean Pricing"
-                           :unit-price-label "Price (per can):"
-                           :unit-label "Can(s):"
-                           :extended-price-label "Extended price:"}}))
+                          {:model {:unit-price 0.65}
+                           :view-model 
+                           {:header "Bean Pricing"
+                            :unit-price-label "Price (per can):"
+                            :unit-label "Can(s):"
+                            :extended-price-label "Extended price:"}}}))
+
+(defn when-bean-unit-price-changed [evt owner state]
+  (om/set-state! owner :unit-price (.. evt -target -value)))
 
 
 (om/root
   (fn [data owner]
-    (reify om/IRender
-      (render [_]
+    (reify 
+      om/IInitState 
+      (init-state [_]
+        (get-in data [:bean-pricing :model])) 
+      om/IRenderState
+      (render-state [this state]
         (html [:div 
-               [:h1 (get-in data [:bean-pricing :header])]
+               [:h1 (get-in data [:bean-pricing :view-model :header])]
                [:div [:span 
-                      [:label {:for :unit-price} (get-in data [:bean-pricing :unit-price-label])]
+                      [:label {:for :unit-price} (get-in data [:bean-pricing :view-model :unit-price-label])]
                       [:input {:type :number
-                               :id :unit-price}]]]
+                               :id :unit-price
+                               :value (:unit-price state)
+                               :on-change #(when-bean-unit-price-changed % owner state)}]]]
                [:div [:span 
-                      [:label {:for :unit} (get-in data [:bean-pricing :unit-label])]
+                      [:label {:for :unit} (get-in data [:bean-pricing :view-model :unit-label])]
                       [:input {:type :number
                                :id :unit}]]]
                [:div [:span 
-                      [:label {:for :extended-price} (get-in data [:bean-pricing :extended-price-label])]
+                      [:label {:for :extended-price} (get-in data [:bean-pricing :view-model :extended-price-label])]
                       [:input {:type :number
                                :id :extended-price}]]]
                ])
@@ -43,3 +54,4 @@
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
+
